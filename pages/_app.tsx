519df4cb137dc44b346ec627cd0e83c0e3e46d1e
TKDchampion <as1219123@yahoo.com.tw>
2022-10-22 14:14:18 +0800

    send transaction

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,8 @@ import type { AppProps } from "next/app";
 import { createWeb3ReactRoot, Web3ReactProvider } from "@web3-react/core";
 import { Web3Provider } from "@ethersproject/providers";
 
-const Web3ProviderNetwork = createWeb3ReactRoot("NETWORK");
-
 function getLibrary(provider: any) {
   const library = new Web3Provider(provider);
-  // library.pollingInterval = 5000
   return library;
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,15 @@
+import { BigNumber } from "@ethersproject/bignumber";
+import { parseUnits } from "@ethersproject/units";
 import { useWeb3React } from "@web3-react/core";
 import type { NextPage } from "next";
+import { useState } from "react";
 import { injected } from "../components/Connectors";
 
 const Home: NextPage = () => {
   const { active, account, library, connector, activate, deactivate } =
     useWeb3React();
+  const [error, setError] = useState<any>();
+  const [txs, setTxs] = useState<any[]>([]);
 
   async function connect() {
     try {
@@ -21,6 +26,54 @@ const Home: NextPage = () => {
       console.log(ex);
     }
   }
+
+  const startPayment = async ({ ether, addr }: any) => {
+    try {
+      if (!window.ethereum)
+        throw new Error("No crypto wallet found. Please install it.");
+      console.log("check 01");
+      await window.ethereum.send("eth_requestAccounts");
+
+      const signer = library.getSigner();
+      console.log("check 02");
+      const tx = signer
+        .sendTransaction({
+          from: account,
+          to: addr,
+          gasLimit: BigNumber.from("21000").toHexString(),
+          value: parseUnits(ether, 18),
+        })
+        .then((response: { hash: any }) => {
+          console.log({ ether, addr });
+          return response.hash;
+        })
+        .catch((error: { code: number }) => {
+          if (error?.code === 4001) {
+            throw new Error("Transaction rejected.");
+          } else {
+            console.error(`Demo Trans Failed`, error, addr, ether);
+
+            throw new Error(`Demo Trans failed`);
+          }
+        });
+      console.log("tx", tx);
+      setTxs([tx]);
+      console.log("check 03");
+    } catch (err: any) {
+      setError(err.message);
+    }
+  };
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    setError("" as any);
+    await startPayment({
+      ether: data.get("ether"),
+      addr: data.get("addr"),
+    });
+  };
+
   return (
     <div className="flex flex-col items-center justify-center">
       <button
@@ -42,6 +95,31 @@ const Home: NextPage = () => {
       >
         Disconnect
       </button>
+
+      <form onSubmit={handleSubmit}>
+        <div className="main-div">
+          <main>
+            <h1>Send ETH payment</h1>
+            <div className="main-div">
+              <div>
+                <input
+                  type="text"
+                  name="addr"
+                  placeholder="Recipient Address"
+                />
+              </div>
+              <div>
+                <input name="ether" type="text" placeholder="Amount in ETH" />
+              </div>
+            </div>
+          </main>
+          <footer className="main-div">
+            <button type="submit">Pay now</button>
+            {error && console.log(error)}
+            {txs && console.log(txs)}
+          </footer>
+        </div>
+      </form>
     </div>
   );
 };
